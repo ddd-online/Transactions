1fff27b5e452564eb24b61a977c1369ba49fc987
ljw <liujingwei1997@outlook.com>
2026-06-17 21:15:48 +0800

    feat: add LedgerID to Tag model and DTO

--- kernel/models/dto/tag_dto.go
+++ kernel/models/dto/tag_dto.go
@@ -3,6 +3,7 @@ package dto
 import "github.com/billadm/models"
 
 type TagDto struct {
+	LedgerID                string `json:"ledgerId"`
 	Name                    string `json:"name"`
 	CategoryTransactionType string `json:"categoryTransactionType"`
 	SortOrder               int    `json:"sortOrder"`
@@ -22,19 +23,17 @@ type UpdateTagSortRequest struct {
 	SortOrder              int    `json:"sortOrder"`
 }
 
-// ToTag 将 TagDto 转换为 Tag
-// 用于将前端传入的数据保存到数据库
 func (dto *TagDto) ToTag() *models.Tag {
 	return &models.Tag{
+		LedgerID:                dto.LedgerID,
 		Name:                    dto.Name,
 		CategoryTransactionType: dto.CategoryTransactionType,
 		SortOrder:               dto.SortOrder,
 	}
 }
 
-// FromTag 从 Tag 模型填充 TagDto
-// 用于将数据库数据转换为前端可用的 DTO
 func (dto *TagDto) FromTag(tag *models.Tag) {
+	dto.LedgerID = tag.LedgerID
 	dto.Name = tag.Name
 	dto.CategoryTransactionType = tag.CategoryTransactionType
 	dto.SortOrder = tag.SortOrder
--- kernel/models/tag.go
+++ kernel/models/tag.go
@@ -1,11 +1,12 @@
 package models
 
 type Tag struct {
-	Name                      string `gorm:"not null;uniqueIndex:idx_tag_category_transaction_type;comment:标签名称" json:"name"`
-	CategoryTransactionType   string `gorm:"not null;uniqueIndex:idx_tag_category_transaction_type;comment:分类:交易类型" json:"category_transaction_type"`
-	SortOrder                 int    `gorm:"default:0;comment:排序顺序" json:"sort_order"`
+	LedgerID                string `gorm:"not null;uniqueIndex:idx_tag_ledger_name_cattype;comment:账本ID" json:"ledger_id"`
+	Name                    string `gorm:"not null;uniqueIndex:idx_tag_ledger_name_cattype;comment:标签名称" json:"name"`
+	CategoryTransactionType string `gorm:"not null;uniqueIndex:idx_tag_ledger_name_cattype;comment:分类:交易类型" json:"category_transaction_type"`
+	SortOrder               int    `gorm:"default:0;comment:排序顺序" json:"sort_order"`
 }
 
-func (tr *Tag) TableName() string {
+func (t *Tag) TableName() string {
 	return "tbl_billadm_tag"
 }
