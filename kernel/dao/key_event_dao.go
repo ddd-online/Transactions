21c1f635b3f5e1ebe0d95f054ccff4c579487d2a
ljw <liujingwei1997@outlook.com>
2026-06-19 19:18:18 +0800

    feat: KeyEvent DAO 查询增加 ledgerID 过滤

--- kernel/dao/key_event_dao.go
+++ kernel/dao/key_event_dao.go
@@ -24,9 +24,9 @@ func GetKeyEventDao() KeyEventDao {
 
 type KeyEventDao interface {
 	UpsertKeyEvent(ws *workspace.Workspace, event *models.KeyEvent) error
-	QueryByDate(ws *workspace.Workspace, date string) (*models.KeyEvent, error)
-	QueryByYear(ws *workspace.Workspace, year string) ([]models.KeyEvent, error)
-	DeleteByDate(ws *workspace.Workspace, date string) error
+	QueryByDate(ws *workspace.Workspace, ledgerID string, date string) (*models.KeyEvent, error)
+	QueryByYear(ws *workspace.Workspace, ledgerID string, year string) ([]models.KeyEvent, error)
+	DeleteByDate(ws *workspace.Workspace, ledgerID string, date string) error
 }
 
 var _ KeyEventDao = &keyEventDaoImpl{}
@@ -37,21 +37,21 @@ func (k *keyEventDaoImpl) UpsertKeyEvent(ws *workspace.Workspace, event *models.
 	return ws.GetDb().Save(event).Error
 }
 
-func (k *keyEventDaoImpl) QueryByDate(ws *workspace.Workspace, date string) (*models.KeyEvent, error) {
+func (k *keyEventDaoImpl) QueryByDate(ws *workspace.Workspace, ledgerID string, date string) (*models.KeyEvent, error) {
 	var event models.KeyEvent
-	err := ws.GetDb().Where("date = ?", date).First(&event).Error
+	err := ws.GetDb().Where("ledger_id = ? AND date = ?", ledgerID, date).First(&event).Error
 	if err != nil {
 		return nil, err
 	}
 	return &event, nil
 }
 
-func (k *keyEventDaoImpl) QueryByYear(ws *workspace.Workspace, year string) ([]models.KeyEvent, error) {
+func (k *keyEventDaoImpl) QueryByYear(ws *workspace.Workspace, ledgerID string, year string) ([]models.KeyEvent, error) {
 	events := make([]models.KeyEvent, 0)
-	err := ws.GetDb().Where("date LIKE ?", year+"-%").Find(&events).Error
+	err := ws.GetDb().Where("ledger_id = ? AND date LIKE ?", ledgerID, year+"-%").Find(&events).Error
 	return events, err
 }
 
-func (k *keyEventDaoImpl) DeleteByDate(ws *workspace.Workspace, date string) error {
-	return ws.GetDb().Where("date = ?", date).Delete(&models.KeyEvent{}).Error
+func (k *keyEventDaoImpl) DeleteByDate(ws *workspace.Workspace, ledgerID string, date string) error {
+	return ws.GetDb().Where("ledger_id = ? AND date = ?", ledgerID, date).Delete(&models.KeyEvent{}).Error
 }
